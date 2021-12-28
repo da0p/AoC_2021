26362afe68e63c93c161e3a5d4d15e4f0ca700fa
Phuong Dao <phuong018@gmail.com>
2021-12-28 16:29:22 -0600

    Add day 21 code

--- day21/dirac_dice.cpp
+++ day21/dirac_dice.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <map>
 
 uint64_t rolls(void) {
 	static uint64_t die = 0;
@@ -7,7 +9,7 @@ uint64_t rolls(void) {
 	return die;
 }
 
-void calcMultiply(uint64_t firstPlayerStartingSpace, uint64_t secondPlayerStartingSpace) {
+uint64_t calcMultiply(uint64_t firstPlayerStartingSpace, uint64_t secondPlayerStartingSpace) {
 
 	bool player1Win = true;
 	uint64_t player1Score = 0;
@@ -42,17 +44,52 @@ void calcMultiply(uint64_t firstPlayerStartingSpace, uint64_t secondPlayerStarti
 	}
 
 	if (player1Win) {
-		std::cout << "player 1 wins, " << player2Score * dice << std::endl;
+		return player2Score * dice;
 	}
 	else {
-		std::cout << "player 2 wins, " << player1Score * dice << std::endl;
+		return player1Score * dice;
 	}
 }
 
+std::pair<uint64_t, uint64_t>  win(uint64_t player1Space, uint64_t player2Space, 
+		uint64_t player1Score, uint64_t player2Score, 
+		std::map<std::vector<uint64_t>, std::pair<uint64_t, uint64_t>> &memoi) {
+
+	if (player1Score >= 21) return  {1, 0};
+
+	if (player2Score >= 21) return {0, 1};
+	
+	if (memoi.count({player1Space, player2Space, player1Score, player2Score}) > 0) {
+		return memoi[{player1Space, player2Space, player1Score, player2Score}];
+	}
+
+	std::pair<uint64_t, uint64_t> winner {0, 0};
+	for (uint64_t i = 1; i <= 3; i++) {
+		for (uint64_t j = 1; j <= 3; j++) {
+			for (uint64_t k = 1; k <= 3; k++) {
+				uint64_t newPlayer1Space = (player1Space + i + j + k - 1) % 10 + 1;
+				uint64_t newPlayer1Score = player1Score + newPlayer1Space;
+				auto who = win(player2Space, newPlayer1Space, 
+						player2Score, newPlayer1Score, memoi);
+				
+				winner.first += who.second;
+				winner.second += who.first;
+			}
+		}
+	}
+	memoi.insert({{player1Space, player2Space, player1Score, player2Score}, winner});
+
+	return winner;
+}
 
 int main() {
 
-	calcMultiply(10, 2);
+	std::cout << "Part 1: " << calcMultiply(10, 2) << std::endl;
+	
+	std::map<std::vector<uint64_t>, std::pair<uint64_t, uint64_t>> memoi;
+	auto winner = win(10, 2, 0, 0, memoi);
+
+	std::cout << "Part 2: " << std::max(winner.first, winner.second) << std::endl;
 
 	return 0;
 }
